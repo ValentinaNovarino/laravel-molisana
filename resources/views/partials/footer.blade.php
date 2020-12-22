69c9ab24611996edcde5a595d5156c9beccd9bbe
ValentinaNovarino <valenovarino@gmail.com>
2020-12-22 17:38:01 +0100

    stilato footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,89 @@
-<footer>
+<footer class="clear-fix">
     <div class="container">
-        <div class="">
-
+        <div class="contact">
+            <img src="{{ asset('images/logo-molisana.png')}}" alt="">
+            <span>Ragione sociale: La Molisana S.P.A.</span>
+            <span>Sede legale: Contrada Colle delle Alpi, 100/A </span>
+            <span>86100 - Campobasso (CB)</span>
+            <span>PEC: lamolisana@pec.it</span>
+            <span>Tel: +39 0874 4981</span>
+            <span>Fax: +39 0874 629584</span>
+            <span>info@lamolisana.it (per segnalazione degli utenti)</span>
+            <span>commerciale@lamolisana.it</span>
+            <span>export@lamolisana.it</span>
+            <span>Numero verde 800818081</span>
+            <span>Telefono 3801292455</span>
+        </div>
+        <div class="left-info">
+            <h2>pastificio</h2>
+            <ul>
+                <li>
+                    <a href="#">Il Pastificio</a>
+                </li>
+                <li>
+                    <a href="#">Grano decorticato a pietra</a>
+                </li>
+                <li>
+                    <a href="#">il Molise c'è</a>
+                </li>
+                <li>
+                    <a href="#">Filiera integrata</a>
+                </li>
+                <li>
+                    <a href="#">100 anni di pasta</a>
+                </li>
+                <li>
+                    <a href="#">Sartoria della pasta</a>
+                </li>
+                <li>
+                    <a href="#">Spaghetto quadrato</a>
+                </li>
+                <li>
+                    <a href="#">Le persone</a>
+                </li>
+            </ul>
+            <h2>prodotti</h2>
+            <ul>
+                <li>
+                    <a href="#">Le Classiche</a>
+                </li>
+                <li>
+                    <a href="#">Le Integrali</a>
+                </li>
+                <li>
+                    <a href="#">Le Speciali</a>
+                </li>
+                <li>
+                    <a href="#">Le Biologiche</a>
+                </li>
+                <li>
+                    <a href="#">Le Gluten-free</a>
+                </li>
+                <li>
+                    <a href="#">Le Semole</a>
+                </li>
+                <li>
+                    <a href="#">Le Extra di Lusso</a>
+                </li>
+            </ul>
+        </div>
+        <div class="right-info">
+            <h2>collezione da chef</h2>
+            <ul>
+                <li>
+                    <a href="#">Collezione da Chef</a>
+                </li>
+                <li>
+                    <a href="#">Grandi Cucine</a>
+                </li>
+                <li>
+                    <a href="#">Biologiche</a>
+                </li>
+                <li>
+                    <a href="#">Quadrate</a>
+                </li>
+            </ul>
         </div>
     </div>
+    <img src="{{ asset('images/montagne-footer.jpg')}}" alt="">
 </footer>
