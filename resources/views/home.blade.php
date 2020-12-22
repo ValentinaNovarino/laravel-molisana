75349176e9ce8c064ce3ca5bb98cac3650d3ba64
ValentinaNovarino <valenovarino@gmail.com>
2020-12-22 14:26:14 +0100

    nuove view + route + collegamento stile e js

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title></title>
+    </head>
+    <body>
+        <h1>Home</h1>
+
+        <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+    </body>
+</html>
