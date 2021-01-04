796f9af12e123b169fb8265003a0aa30bad2d19e
ValentinaNovarino <valenovarino@gmail.com>
2021-01-04 16:41:41 +0100

    diviso pasta per tipo

---
+++ resources/views/details.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.app')
+@section ('content')
+    <h1>Details</h1>
+@endsection
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,14 +1,19 @@
 @extends('layout.app')
 @section ('content')
-    <div class="container-card">
-        @foreach ($formati as $formato)
-            <div class="card">
-                <img src="{{ $formato['src'] }}" alt="{{ $formato['titolo'] }}">
-                <div class="overlay">
-                    <a href="#">
-                        {{ $formato['titolo'] }}
-                    </a>
-                </div>
+    <div class="container">
+        @foreach ($formati as $tipo => $pasta)
+            <h1>{{ $tipo }}</h1>
+            <div class="container-card">
+                @foreach ($pasta as $indice => $formato)
+                    <div class="card">
+                        <img src="{{ $formato['src'] }}" alt="{{ $formato['titolo'] }}">
+                        <div class="overlay">
+                            <a href="#">
+                                {{ $formato['titolo'] }}
+                            </a>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         @endforeach
     </div>
